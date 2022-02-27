54ebb017386eae7d44060f8b39616c69c7d37307
Sup3r-Us3r <maydersonmello@gmail.com>
2022-02-27 13:58:21 -0300

    challenge

--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,3 +1,7 @@
 defmodule Flightex do
+  alias Flightex.Users.CreateOrUpdate, as: CreateOrUpdateUser
+  alias Flightex.Bookings.CreateOrUpdate, as: CreateOrUpdateBooking
 
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_booking(params), to: CreateOrUpdateBooking, as: :call
 end
--- lib/users/user.ex
+++ lib/users/user.ex
@@ -3,7 +3,17 @@ defmodule Flightex.Users.User do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(name, email, cpf) when is_bitstring(cpf) do
+    {:ok,
+     %__MODULE__{
+       name: name,
+       email: email,
+       cpf: cpf,
+       id: UUID.uuid4()
+     }}
+  end
+
+  def build(_name, _email, cpf) when is_number(cpf) do
+    {:error, "Cpf must be a String"}
   end
 end
